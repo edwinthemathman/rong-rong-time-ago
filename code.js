03257ba1145d13561d1420109cef8a693cdcfa12
edwinthemathman <157263911+edwinthemathman@users.noreply.github.com>
2026-01-21 21:01:33 -0800

    Add files via upload

---
+++ code.js
@@ -0,0 +1,25 @@
+let joseph = 0;
+const josephButton = document.getElementById("joseph");
+const josephButton2 = document.getElementById("joseph2");
+const josephCounter = document.getElementById("joseph-counter");
+const josephConfirm = document.getElementById("josephConfirm");
+
+click(1, josephButton);
+
+function click(increment, buttonName) {
+  buttonName.addEventListener('click', () => {
+    joseph += increment;
+    josephCounter.textContent = `Joseph count: ${joseph}`;
+    
+    if (joseph >= 20) {
+      josephConfirm.textContent = "Unlock more joseph?";
+    }
+  });
+}
+
+josephConfirm.addEventListener('click', () => {
+  josephConfirm.textContent = "";
+  josephButton2.textContent = "MORE JOSEPH!";
+});
+
+click(20, josephButton2);
